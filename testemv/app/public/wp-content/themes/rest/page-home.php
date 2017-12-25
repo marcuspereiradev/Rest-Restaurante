b0e66ec794db32423b9af24a2f3d3de727aad313
marcuspereiragithub <vinnycoast@gmail.com>
2017-12-24 23:51:24 -0200

    Removing some code and adding plugins

--- testemv/app/public/wp-content/themes/rest/page-home.php
+++ testemv/app/public/wp-content/themes/rest/page-home.php
@@ -1,13 +1,82 @@
 <?php
 // Template Name: Menu da Semana
 ?>
-
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<section class="container">
+			<h2 class="subtitulo"><?php the_title(); ?></h2>
+
+                <div class="menu-item grid-8">
+                    <h2><?php the_field('nome_categoria'); ?></h2>
+                    <ul>
+                        <li>
+                            <span><sup>R$</sup><?php the_field('preco_prato'); ?></span>
+                            <div>
+                                <h3><?php the_field('nome_prato'); ?></h3>
+                                <?php the_field('descricao_prato'); ?>
+                            </div>
+                        </li>
+                        
+                        <li>
+                            <span><sup>R$</sup><?php the_field('preco_prato'); ?></span>
+                            <div>
+                                <h3><?php the_field('nome_prato'); ?></h3>
+                                <?php the_field('descricao_prato'); ?>
+                            </div>
+                        </li>
+                        
+                        <li>
+                            <span><sup>R$</sup><?php the_field('preco_prato'); ?></span>
+                            <div>
+                                <h3><?php the_field('nome_prato'); ?></h3>
+                                <?php the_field('descricao_prato'); ?>
+                            </div>
+                        </li>
+                        
+                    </ul>
+                </div>
+                
+                <div class="menu-item grid-8">
+                    <h2><?php the_field('nome_categoria_2'); ?></h2>
+                    <ul>
+                        <li>
+                            <span><sup>R$</sup><?php the_field('preco_prato_2'); ?></span>
+                            <div>
+                                <h3><?php the_field('nome_prato_2'); ?></h3>
+                                <?php the_field('descricao_prato_2'); ?>
+                            </div>
+                        </li>
+                        
+                        <li>
+                            <span><sup>R$</sup><?php the_field('preco_prato_2'); ?></span>
+                            <div>
+                                <h3><?php the_field('nome_prato_2'); ?></h3>
+                                <?php the_field('descricao_prato_2'); ?>
+                            </div>
+                        </li>
+                        
+                        <li>
+                            <span><sup>R$</sup><?php the_field('preco_prato_2'); ?></span>
+                            <div>
+                                <h3><?php the_field('nome_prato_2'); ?></h3>
+                                <?php the_field('descricao_prato_2'); ?>
+                            </div>
+                        </li>
+                        
+                    </ul>
+                </div>
+                
+		</section>
+<?php endwhile; else: endif; ?>
+<?php get_footer(); ?>
+
+
+<!--
+
 
 <section class="container">
-    <h2 class="subtitulo"><?php the_title(); ?></h2>
+    <h2 class="subtitulo"></h2>
 
     <div class="menu-item grid-8">
         <h2>Peixes</h2>
@@ -65,6 +134,6 @@
 
 </section>
 
-<?php endwhile; else: endif; ?>
 
-<?php get_footer(); ?>
+
+-->
